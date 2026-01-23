e438c24132f20d408be0cd58389800c86dfe85fc
Kingsley245 <kingsleyfestus24@gmail.com>
2026-01-24 00:13:29 +0100

    another commit

--- app/components/metheHOD.tsx
+++ app/components/metheHOD.tsx
@@ -1,4 +1,5 @@
 import { motion } from 'framer-motion';
+import HOD from '../Assets/HOD.png';
 
 export default function MeetTheHOD() {
   return (
@@ -16,18 +17,18 @@ export default function MeetTheHOD() {
             {/* Decorative Background Element */}
             <div className="absolute -inset-4 bg-blue-600/10 rounded-2xl -rotate-3 group-hover:rotate-0 transition-transform duration-500" />
 
-            <div className="relative z-10 rounded-2xl overflow-hidden shadow-2xl border-8 border-white">
+            <div className="relative z-10 rounded-2xl overflow-hidden shadow-2xl border-4 md:border-8 border-white">
               <img
-                src={''} // Replace with actual HOD image
+                src={HOD}
                 alt="Head of Department"
-                className="w-full h-125 object-cover object-top scale-105 group-hover:scale-100 transition-transform duration-700"
+                className="w-full h-72 sm:h-96 md:h-125 object-cover object-top scale-105 group-hover:scale-100 transition-transform duration-700"
               />
               {/* Overlay Label */}
-              <div className="absolute bottom-0 left-0 right-0 bg-linear-to-t from-blue-900 to-transparent p-8">
-                <p className="text-white/80 text-sm font-bold tracking-widest uppercase">
+              <div className="absolute bottom-0 left-0 right-0 bg-linear-to-t from-blue-900/90 to-transparent p-4 md:p-8">
+                <p className="text-white/80 text-[10px] md:text-sm font-bold tracking-widest uppercase">
                   Departmental Leadership
                 </p>
-                <h3 className="text-white text-2xl font-black">
+                <h3 className="text-white text-lg md:text-2xl font-black">
                   Prof. Dr. Chen Kwesi
                 </h3>
               </div>
--- app/lib/lecturerData.tsx
+++ app/lib/lecturerData.tsx
@@ -1,26 +1,30 @@
+import LI from '../Assets/lecture 1.png';
+import L2 from '../Assets/lecture 2.png';
+import L3 from '../Assets/lecture 3.png';
+
 export const lecturers = [
   {
     id: 1,
     name: 'Prof. Lucian Manroe',
     roles: ['Head of Research', 'Power Systems'], // Array for multiple roles
-    img: '',
+    img: LI,
   },
   {
     id: 2,
     name: 'Dr. Edward James',
     roles: ['Senior Lecturer', 'Embedded Systems', 'Patents Lead'],
-    img: '',
+    img: L2,
   },
   {
     id: 3,
     name: 'Engr. Hanna',
     roles: ['Lab Coordinator', 'Control Engineering'],
-    img: '',
+    img: L3,
   },
   {
     id: 4,
     name: 'Engr. Kingsley festus',
     roles: ['software developer', 'Control Engineering', 'React developer'],
-    img: '',
+    img: L3,
   },
 ];
